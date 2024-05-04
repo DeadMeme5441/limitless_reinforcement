63fdb2db00f01ab10a8ed4bd32afc939e5f082b1
DeadMeme5441 <deadunderscorememe@gmail.com>
2024-05-04 20:08:41 +0530

    Markov Stock Simulation Commented

--- rl/markov_processes/stock_price_markov.py
+++ rl/markov_processes/stock_price_markov.py
@@ -14,18 +14,40 @@ import itertools
 
 @dataclass(frozen=True)
 class StateMP1:
+    """
+    For our first process, we assume our state to be just the price.
+    Hence S[t] = (X[t])
+    where X[t] is the Price at time t.
+    """
+
     price: int
 
 
 @dataclass
 class StockPriceMP1(MarkovProcess[StateMP1]):
+    """
+    The assumption for the first process is that the price tends to revert to
+    a mean that is defined.
+
+    Level is the average level that the price tends to "mean revert" to.
+    Alpha1 is the "strength" or magnitude of this mean-reverting behaviour.
+    """
+
     level_param: int
     alpha1: float = 0.25
 
     def up_prob(self, state: StateMP1) -> float:
+        """
+        The model that we implement for the first process is
+        P(S[t+1] | S[t]) = 1.0 / (1 + e ^ ((-alpha1) * (level - X[t])))
+        """
         return get_logistic_func(self.alpha1)(self.level_param - state.price)
 
     def transition(self, state: NonTerminal[StateMP1]) -> Categorical[State[StateMP1]]:
+        """
+        The transition probabilities for the states given the probability that the price goes up
+        is generated for both Price[t] + 1 as up_probability and Price[t] - 1 as 1 - up_probability.
+        """
         up_p = self.up_prob(state.state)
         return Categorical(
             {
@@ -40,18 +62,40 @@ handy_map: Mapping[Optional[bool], int] = {True: -1, False: 1, None: 0}
 
 @dataclass(frozen=True)
 class StateMP2:
+    """
+    For the second process, we utilize 2 state variables - Price[t] and a boolean is_prev_move_up,
+    which represents if the last price movement was up or down.
+    Hence S[t] = (X[t], X[t] - X[t-1])
+    where X[t] is the Price at time t.
+    """
+
     price: int
     is_prev_move_up: Optional[bool]
 
 
 @dataclass
 class StockPriceMP2(MarkovProcess[StateMP2]):
+    """
+    The model we implement for the second process assumes that the price will move in reverse bias
+    to the last price.
+    Alpha2 is magnitude by which this reverse bias occurs.
+    """
+
     alpha2: float = 0.75
 
     def up_prob(self, state: StateMP2) -> float:
+        """
+        The model that we implement for the second process is
+        P(S[t+1] | S[t]) = 0.5 * (1 + alpha2 * (direction of previous move)) if t > 0
+        and P(S[t+1] | S[t]) = 0.5 if t = 0
+        """
         return 0.5 * (1 + self.alpha2 * handy_map[state.is_prev_move_up])
 
     def transition(self, state: NonTerminal[StateMP2]) -> Categorical[State[StateMP2]]:
+        """
+        The transition probabilities for the states given the probability that the price goes up
+        is generated for both Price[t] + 1 as up_probability and Price[t] - 1 as 1 - up_probability.
+        """
         up_p = self.up_prob(state.state)
         return Categorical(
             {
@@ -68,15 +112,40 @@ class StockPriceMP2(MarkovProcess[StateMP2]):
 
 @dataclass(frozen=True)
 class StateMP3:
+    """
+    For the third process, we utilize 2 state variables - num_up_moves and num_down_moves.
+    These 2 variables represent the number of up_moves and down_moves that the price has taken
+    from the beginning.
+    U[t] = Sum[i=1,t](max(X[i] - X[i - 1], 0))
+    D[t] = Sum[i=1,t](max(X[i - 1] - X[i], 0))
+    and,
+    S[t] = (U[t], D[t])
+    where,
+    U[t] is the number of up moves.
+    D[t] is the number of down moves.
+    """
+
     num_up_moves: int
     num_down_moves: int
 
 
 @dataclass
 class StockPriceMP3(MarkovProcess[StateMP3]):
+    """
+    The model we implement for the third process assumes that the price will depend on
+    all past movements. Specifically, it depends on number of past up moves relative to the
+    number of past down moves.
+    Alpha3 is magnitude by which this bias occurs.
+    """
+
     alpha3: float = 1.0
 
     def up_prob(self, state: StateMP3) -> float:
+        """
+        The model that we implement for the third process is
+        P(S[t+1] | S[t]) =  1 / ( 1 + ( ( U[t] + D[t] ) / D[t] - 1 ) ^ alpha3 ) if t > 0
+        P(S[t+1] | S[t]) = 0.5 if t = 0
+        """
         total = state.num_down_moves + state.num_up_moves
 
         return (
@@ -86,6 +155,10 @@ class StockPriceMP3(MarkovProcess[StateMP3]):
         )
 
     def transition(self, state: NonTerminal[StateMP3]) -> Categorical[State[StateMP3]]:
+        """
+        The transition probabilities for the states given the probability that the price goes up
+        is generated for both Price[t] + 1 as up_probability and Price[t] - 1 as 1 - up_probability.
+        """
         up_p = self.up_prob(state.state)
         return Categorical(
             {
@@ -103,16 +176,25 @@ class StockPriceMP3(MarkovProcess[StateMP3]):
 def process1_price_traces(
     start_price: int, level_param: int, alpha1: float, time_steps: int, num_traces: int
 ) -> np.ndarray:
+    """
+    This function generates the price traces for Process 1.
+    """
     mp = StockPriceMP1(level_param, alpha1)
+    # Instantiate a Distribution with a single outcome of probability 1.
     start_state_distribution = Constant(NonTerminal(StateMP1(start_price)))
 
+    # Generate the number of traces as passed as parameter.
     return np.vstack(
         [
+            # Iterate over all simulated price changes given a single state
+            # from t = 1 to time steps.
             np.fromiter(
                 (
                     s.state.price
                     for s in itertools.islice(
-                        mp.simulate(start_state_distribution), time_steps + 1
+                        # Simulate the markov process as given by the transition function.
+                        mp.simulate(start_state_distribution),
+                        time_steps + 1,
                     )
                 ),
                 float,
@@ -125,15 +207,25 @@ def process1_price_traces(
 def process2_price_traces(
     start_price: int, alpha2: float, time_steps: int, num_traces: int
 ) -> np.ndarray:
+    """
+    This function generates the price traces for Process 2.
+    """
     mp = StockPriceMP2(alpha2)
+    # Instantiate a Distribution with a single outcome of probability 1.
     start_state_distribution = Constant(NonTerminal(StateMP2(start_price, None)))
+
+    # Generate the number of traces as passed as parameter.
     return np.vstack(
         [
+            # Iterate over all simulated price changes given a single state
+            # from t = 1 to time steps.
             np.fromiter(
                 (
                     s.state.price
                     for s in itertools.islice(
-                        mp.simulate(start_state_distribution), time_steps + 1
+                        # Simulate the markov process as given by the transition function.
+                        mp.simulate(start_state_distribution),
+                        time_steps + 1,
                     )
                 ),
                 float,
@@ -146,17 +238,27 @@ def process2_price_traces(
 def process3_price_traces(
     start_price: int, alpha3: float, time_steps: int, num_traces: int
 ) -> np.ndarray:
+    """
+    This function generates the price traces for Process 2.
+    """
     mp = StockPriceMP3(alpha3)
+    # Instantiate a Distribution with a single outcome of probability 1.
     start_state_distribution = Constant(
         NonTerminal(StateMP3(num_up_moves=0, num_down_moves=0))
     )
+
+    # Generate the number of traces as passed as parameter.
     return np.vstack(
         [
+            # Iterate over all simulated price changes given a single state
+            # from t = 1 to time steps.
             np.fromiter(
                 (
                     start_price + s.state.num_up_moves - s.state.num_down_moves
                     for s in itertools.islice(
-                        mp.simulate(start_state_distribution), time_steps + 1
+                        # Simulate the markov process as given by the transition function.
+                        mp.simulate(start_state_distribution),
+                        time_steps + 1,
                     )
                 ),
                 float,
